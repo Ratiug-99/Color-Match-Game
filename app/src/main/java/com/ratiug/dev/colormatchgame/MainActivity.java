4896d4c5cec5004e259e25e4d48ed59a300e8d6c
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-10-10 13:25:23 +0300

    create design main activity - Right answer value, Wrong answer value, and record value

--- app/src/main/java/com/ratiug/dev/colormatchgame/MainActivity.java
+++ app/src/main/java/com/ratiug/dev/colormatchgame/MainActivity.java
@@ -3,8 +3,11 @@ package com.ratiug.dev.colormatchgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvNameRight,tvNameWrong,tvRightValue,tvWrongValue,tvRecordValue;
+    public int rightAnswer,wrongAnswer,record;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
