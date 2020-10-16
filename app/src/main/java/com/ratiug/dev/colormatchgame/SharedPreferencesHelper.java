d20eccd5b99654e388df4f0a768b2c827b2f9166
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-10-16 06:04:17 +0300

    set options count colors to SharedPreferencesHelper

--- app/src/main/java/com/ratiug/dev/colormatchgame/SharedPreferencesHelper.java
+++ app/src/main/java/com/ratiug/dev/colormatchgame/SharedPreferencesHelper.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 public class SharedPreferencesHelper {
     public static final String APP_PREFERENCES_NAME = "APP_PREFERENCES_NAME";
     public static final String APP_PREFERENCES_RECORD = "APP_PREFERENCES_RECORD";
+    public static final String APP_PREFERENCE_COUNT_COLORS = "APP_PREFERENCE_COUNT_COLORS";
 
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
@@ -19,9 +20,17 @@ public class SharedPreferencesHelper {
     public int getRecord(){
       return  mSharedPreferences.getInt(APP_PREFERENCES_RECORD,0);
     }
-
     public void setRecord(int record){
         mEditor.putInt(APP_PREFERENCES_RECORD,record);
+        mEditor.apply();
+    }
+
+    public int getCountColors(){
+        return mSharedPreferences.getInt(APP_PREFERENCE_COUNT_COLORS, 0);
+    }
+
+    public void setCountColors(int count){
+        mEditor.putInt(APP_PREFERENCE_COUNT_COLORS,count);
         mEditor.apply();
     }
 }
--- app/src/main/java/com/ratiug/dev/colormatchgame/activities/SettingsActivity.java
+++ app/src/main/java/com/ratiug/dev/colormatchgame/activities/SettingsActivity.java
@@ -3,14 +3,45 @@ package com.ratiug.dev.colormatchgame.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.ratiug.dev.colormatchgame.R;
+import com.ratiug.dev.colormatchgame.SharedPreferencesHelper;
 
 public class SettingsActivity extends AppCompatActivity {
+    Spinner spinnerCountColors;
+    SharedPreferencesHelper sharedPreferencesHelper;
+    Integer selected_count_colors;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        sharedPreferencesHelper = new SharedPreferencesHelper(this);
+        selected_count_colors = sharedPreferencesHelper.getCountColors();
+
+        spinnerCountColors = findViewById(R.id.s_colors_count);
+        ArrayAdapter <?> adapter = ArrayAdapter.createFromResource(this, R.array.count_colors_for_select,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerCountColors.setAdapter(adapter);
+
+       spinnerCountColors.setSelection(selected_count_colors);
+
+        spinnerCountColors.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                sharedPreferencesHelper.setCountColors(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
