e933df748c4b6fb9c8ab642d33f83d3924c931dc
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-11-11 22:16:20 +0200

    Checking database record in SplashScreen

--- app/src/main/java/com/ratiug/dev/colormatchgame/activities/WelcomeActivity.java
+++ app/src/main/java/com/ratiug/dev/colormatchgame/activities/WelcomeActivity.java
@@ -8,17 +8,29 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ratiug.dev.colormatchgame.R;
 import com.ratiug.dev.colormatchgame.SharedPreferencesHelper;
+import com.ratiug.dev.colormatchgame.User;
 import com.ratiug.dev.colormatchgame.UserDao;
+
+import java.util.Objects;
+
 
 public class WelcomeActivity extends AppCompatActivity {
     public static final String TAG = "DBG | SplashScreen ";
     UserDao userDao = new UserDao();
+    DatabaseReference reference;
     private SharedPreferencesHelper mSharedPreferencesHelper;
 
     @Override
@@ -35,37 +47,72 @@ public class WelcomeActivity extends AppCompatActivity {
             public void run() {
                 selectActivity();
             }
-        }, 1000);
+        }, 1);
 
     }
 
 
-
-
     private void selectActivity() {
         if (mSharedPreferencesHelper.getTokenId() != null) {
-            userDao.updateUserInfo(this);
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            checkRecord();
         } else {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+            openLoginActivity();
         }
+
+    }
+
+    private void checkRecord() {
+        reference = FirebaseDatabase.getInstance().getReference().child("Users:");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    User p = dataSnapshot1.getValue(User.class);
+                    String cUserId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
+                    String lUser = p.getUid();
+                    if (p.getUid() != null && cUserId.equals(lUser)) {
+                        int bdUserRecord = Integer.parseInt(p.getRecord());
+                        int record = mSharedPreferencesHelper.getRecord();
+                        if (bdUserRecord > record) {
+                            mSharedPreferencesHelper.setRecord(bdUserRecord);
+                            openMainActivity();
+                        } else {
+                            openMainActivity();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
         Animatoo.animateFade(this);
-//        Animatoo.animateSplit(this);
+        finish();
+    }
+
+    private void openMainActivity() {
+        userDao.updateUserInfo(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        Animatoo.animateFade(this);
         finish();
     }
 
     private void setThemeApp() {
         AppCompatDelegate.setDefaultNightMode(mSharedPreferencesHelper.getTheme());
-
-        Resources res = getResources();
-        boolean enableQAurl = res.getBoolean(R.bool.windowLightStatusBarValue);
     }
 
     private void checkLocale() {
         String prefLanguage = mSharedPreferencesHelper.getLanguage().trim(); // check on first load language and set it to SP
-        Log.d(TAG, "checkLocale: "  + prefLanguage);
+        Log.d(TAG, "checkLocale: " + prefLanguage);
         if (prefLanguage.equals("")) {
             Log.d(TAG, "checkLocale: ");
             mSharedPreferencesHelper.setLanguage(getResources().getConfiguration().locale.toString().substring(0, 2));
